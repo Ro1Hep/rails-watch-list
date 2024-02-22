248331aa9b40e4f4ebb00949d5c4fbabb6df23ff
Rowan Heptinstall <rowanheptinstall1@gmail.com>
2024-02-22 17:40:01 +0000

    Add bookmark functions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,16 +10,28 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(permit)
+    list = List.find(params[:list_id])
+    @bookmark.list = list
     if @bookmark.save
-      redirect_to list_path(@bookmark[:list_id])
+      redirect_to list_path(params[:list_id])
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark = find_bookmark
+    @bookmark.destroy
+    redirect_to list_path(params[:list_id])
   end
 
   private
 
   def permit
     params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+
+  def find_bookmark
+    Bookmark.find(params[:id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = find_list
+    # @bookmarks = Bookmark.find(@list.bookmarks.id)
   end
 
   def new
